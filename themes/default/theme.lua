be9d94c1e84e99b8fcd9cd3015933079a986dc78
Eugene Che <eugene.y.che@gmail.com>
2013-07-23 20:01:12 -0700

    Updated menubar with widgets and icons

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -1,103 +1,80 @@
----------------------------
--- Default awesome theme --
----------------------------
-
 theme = {}
 
-theme.font          = "Termsynu 11"
-theme.wallpaper = "~/Pictures/Wallpapers/daftpunk2.jpg"
+dir					= "~/.config/awesome/themes/default/"
+theme.font      			= "Termsynu 11"
+theme.wallpaper 			= "~/Pictures/Wallpapers/panda.png"
 
-theme.tasklist_disable_icon = true
+theme.tasklist_disable_icon 		= true
 
-theme.bg_normal     = "#141414"
-theme.bg_focus      = "#222222"
-theme.bg_urgent     = "#ff0000"
-theme.bg_minimize   = "#22222200"
-theme.bg_systray    = "#00000000"
+theme.bg_normal     			= "#141414"
+theme.bg_focus      			= "#222222"
+theme.bg_urgent				= "#ff0000"
+theme.bg_minimize   			= "#22222200"
+theme.bg_systray    			= "#00000000"
 
-theme.fg_normal     = "#888888"
-theme.fg_focus      = "#cccccc"
-theme.fg_urgent     = "#dddddd"
-theme.fg_minimize   = "#666666"
+theme.fg_normal     			= "#888888"
+theme.fg_focus      			= "#cccccc"
+theme.fg_urgent     			= "#dddddd"
+theme.fg_minimize   			= "#666666"
 
-theme.border_width  = 3
-theme.border_normal = "#141414"
-theme.border_focus  = "#141414"
-theme.border_marked = "#91231c"
+theme.border_width  			= 3
+theme.border_normal 			= "#141414"
+theme.border_focus  			= "#141414"
+theme.border_marked			= "#91231c"
 
--- There are other variable sets
--- overriding the default one when
--- defined, the sets are:
+theme.menu_height			= 20
+theme.menu_width			= 150
+theme.menu_border_width			= 0
+
+theme.taglist_squares_sel   		= dir .. "taglist/squarefw.png"
+theme.taglist_squares_unsel 		= dir .. "taglist/squarew.png"
+
+theme.awesome_icon			= dir .. "icons/widgets/awesome.png"
+theme.menu_submenu_icon 		= dir .. "submenu.png"
+
+theme.app_icon 				= dir .. "icons/menu/apps.png"
+theme.pref_icon 		  	= dir .. "icons/menu/pref.png"
+
+theme.widget_background			= "#090909"
+
+theme.seperator_icon 		  	= dir .. "icons/widgets/seperator.png"
+theme.seperator_with_padding_icon 	= dir .. "icons/widgets/seperator_with_padding.png"
+
+theme.batt_charging_icon		= dir .. "icons/widgets/batt_charging.png"  
+theme.batt_discharging_icon		= dir .. "icons/widgets/batt_discharging.png"  
+theme.batt_color			= "#AECF96"
+theme.batt_low_color			= "#DD4940"
+
+theme.cpu_icon				= dir .. "icons/widgets/cpu.png"
+
+theme.wifi_hi_icon			= dir .. "icons/widgets/wifi_hi.png"
+theme.wifi_med_icon			= dir .. "icons/widgets/wifi_med.png"
+theme.wifi_low_icon			= dir .. "icons/widgets/wifi_low.png"
+theme.wifi_no_icon			= dir .. "icons/widgets/wifi_no.png"
+
+theme.titlebar_close_button_normal 	= dir .. "titlebar/close_normal.png"
+theme.titlebar_close_button_focus  	= dir .. "titlebar/close_focus.png"
+
+theme.layout_fairh 			= dir .. "layouts/fairh.png"
+theme.layout_fairv 			= dir .. "layouts/fairv.png"
+theme.layout_floating  			= dir .. "layouts/floating.png"
+theme.layout_magnifier 			= dir .. "layouts/magnifier.png"
+theme.layout_max 			= dir .. "layouts/max.png"
+theme.layout_fullscreen 		= dir .. "layouts/fullscreen.png"
+theme.layout_tilebottom 		= dir .. "layouts/tilebottom.png"
+theme.layout_tileleft   		= dir .. "layouts/tileleft.png"
+theme.layout_tile 			= dir .. "layouts/tileright.png"
+theme.layout_tiletop 			= dir .. "layouts/tiletop.png"
+theme.layout_spiral  			= dir .. "layouts/spiral.png"
+theme.layout_dwindle 			= dir .. "layouts/dwindle.png"
+
+theme.icon_theme 			= "/usr/share/icons/Faenza"
+
+return theme
+
 -- [taglist|tasklist]_[bg|fg]_[focus|urgent]
 -- titlebar_[bg|fg]_[normal|focus]
 -- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
 -- mouse_finder_[color|timeout|animate_timeout|radius|factor]
--- Example:
---theme.taglist_bg_focus = "#ff0000"
-
--- Display the taglist squares
-theme.taglist_squares_sel   = "~/.config/awesome/themes/default/taglist/squarefw.png"
-theme.taglist_squares_unsel = "~/.config/awesome/themes/default/taglist/squarew.png"
-
--- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-
-theme.menu_submenu_icon = "~/.config/awesome/themes/default/submenu.png"
-theme.menu_height = 19
-theme.menu_width  = 150
-theme.menu_border_width=0
-
--- You can add as many variables as
--- you wish and access them by using
--- beautiful.variable in your rc.lua
---theme.bg_widget = "#cc0000"
-
--- Define the image to load
-theme.titlebar_close_button_normal = "~/.config/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "~/.config/awesome/themes/default/titlebar/close_focus.png"
-
---theme.titlebar_ontop_button_normal_inactive = "~/.config/awesome/default/titlebar/ontop_normal_inactive.png"
---theme.titlebar_ontop_button_focus_inactive  = "~/.config/awesome/themes/default/titlebar/ontop_focus_inactive.png"
---theme.titlebar_ontop_button_normal_active = "~/.config/awesome/themes/default/titlebar/ontop_normal_active.png"
---theme.titlebar_ontop_button_focus_active  = "~/.config/awesome/themes/default/titlebar/ontop_focus_active.png"
-
---theme.titlebar_sticky_button_normal_inactive = "~/.config/awesome/themes/default/titlebar/sticky_normal_inactive.png"
---theme.titlebar_sticky_button_focus_inactive  = "~/.config/awesome/themes/default/titlebar/sticky_focus_inactive.png"
---theme.titlebar_sticky_button_normal_active = "~/.config/awesome/themes/default/titlebar/sticky_normal_active.png"
---theme.titlebar_sticky_button_focus_active  = "~/.config/awesome/themes/default/titlebar/sticky_focus_active.png"
-
---theme.titlebar_floating_button_normal_inactive = "~/.config/awesome/themes/default/titlebar/floating_normal_inactive.png"
---theme.titlebar_floating_button_focus_inactive  = "~/.config/awesome/themes/default/titlebar/floating_focus_inactive.png"
---theme.titlebar_floating_button_normal_active = "~/.config/awesome/themes/default/titlebar/floating_normal_active.png"
---theme.titlebar_floating_button_focus_active  = "~/.config/awesome/themes/default/titlebar/floating_focus_active.png"
-
---theme.titlebar_maximized_button_normal_inactive = "~/.config/awesome/themes/default/titlebar/maximized_normal_inactive.png"
---theme.titlebar_maximized_button_focus_inactive  = "~/.config/awesome/themes/default/titlebar/maximized_focus_inactive.png"
---theme.titlebar_maximized_button_normal_active = "~/.config/awesome/themes/default/titlebar/maximized_normal_active.png"
---ztheme.titlebar_maximized_button_focus_active  = "~/.config/awesome/themes/default/titlebar/maximized_focus_active.png"
-
--- You can use your own layout icons like this:
-theme.layout_fairh = "~/.config/awesome/themes/default/newlayouts/fairh.png"
-theme.layout_fairv = "~/.config/awesome/themes/default/newlayouts/fairv.png"
-theme.layout_floating  = "~/.config/awesome/themes/default/newlayouts/floating.png"
-theme.layout_magnifier = "~/.config/awesome/themes/default/newlayouts/magnifier.png"
-theme.layout_max = "~/.config/awesome/themes/default/newlayouts/max.png"
-theme.layout_fullscreen = "~/.config/awesome/themes/default/newlayouts/fullscreen.png"
-theme.layout_tilebottom = "~/.config/awesome/themes/default/newlayouts/tilebottom.png"
-theme.layout_tileleft   = "~/.config/awesome/themes/default/newlayouts/tileleft.png"
-theme.layout_tile = "~/.config/awesome/themes/default/newlayouts/tileright.png"
-theme.layout_tiletop = "~/.config/awesome/themes/default/newlayouts/tiletop.png"
-theme.layout_spiral  = "~/.config/awesome/themes/default/newlayouts/spiral.png"
-theme.layout_dwindle = "~/.config/awesome/themes/default/newlayouts/dwindle.png"
-
-theme.awesome_icon = "~/.config/awesome/themes/default/icons/awesome.png"
-theme.app_icon = "~/.config/awesome/themes/default/icons/apps.png"
-theme.pref_icon = "~/.config/awesome/themes/default/icons/pref.png"
-
--- Define the icon theme for application icons. If not set then the icons 
--- from /usr/share/icons and /usr/share/icons/hicolor will be used.
-theme.icon_theme = nil
-
-return theme
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
--- widgets.lua
+++ widgets.lua
@@ -1,36 +1,63 @@
-seperator = wibox.widget.textbox()
-seperator:set_text(" | ");
+require("beautiful")
+
+seperator = wibox.widget.imagebox()
+seperator:set_image(beautiful.seperator_icon)
 
 datewidget = wibox.widget.textbox()
 vicious.register(datewidget, vicious.widgets.date, " %a %b %d, %H:%M ", 60)
 
-battwidgettext = wibox.widget.textbox()
-vicious.register(battwidgettext, vicious.widgets.bat,
-	function (widget, args)
-		if args[2] == 0 then return "Battery derped" end
-        text = args[3]
+batticon = wibox.widget.imagebox()
+batticon:set_image(beautiful.batt_charging)
+vicious.register(batticon, vicious.widgets.bat,
+    function (widget, args)
         if (args[1] == "-") then
-	    return "[-]BATT "
+	    widget:set_image(beautiful.batt_discharging_icon)
         else
-	    return "[+]BATT "
+	    widget:set_image(beautiful.batt_charging_icon)
 	end
-	end, 1, "BAT0"
+	return ""
+    end, 1, "BAT0"
 )
  
 battwidget = awful.widget.progressbar()
 battwidget:set_width(8)
-battwidget:set_height(10)
+battwidget:set_height(20)
 battwidget:set_vertical(true)
-battwidget:set_background_color("#494B4F")
-battwidget:set_color("#AECF96")
-vicious.register(battwidget, vicious.widgets.bat, "$2", 1, "BAT0")
+battwidget:set_background_color(beautiful.widget_background)
+battwidget:set_color(beautiful.batt_color)
+vicious.register(battwidget, vicious.widgets.bat,
+    function (widget, args)
+        if(args[2] > 30) then
+	    widget:set_color(beautiful.batt_color)
+	else
+            widget:set_color(beautiful.batt_low_color)
+        end
+        return args[2]
+    end, 1, "BAT0")
+
+cpuicon = wibox.widget.imagebox()
+cpuicon:set_image(beautiful.cpu_icon)
 
 cpuwidget = awful.widget.graph()
 cpuwidget:set_width(50)
-cpuwidget:set_background_color("#494B4F")
-cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, 
-                    {1, "#AECF96" }}})
+cpuwidget:set_background_color(beautiful.widget_background)
+cpuwidget:set_color("#AECF96")
 vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
 
+wifiicon = wibox.widget.imagebox()
+vicious.register(wifiicon, vicious.widgets.wifi,
+    function (widget, args)
+        link = args['{link}']
+	if link > 70 then
+	    widget:set_image(beautiful.wifi_hi_icon)
+	elseif link > 30 and link <= 70 then
+	    widget:set_image(beautiful.wifi_med_icon)
+	elseif link > 0 and link <= 30 then
+	    widget:set_image(beautiful.wifi_low_icon)
+	else
+	    widget:set_image(beautiful.widget_wifi_no)
+	end
+    end, 3, "wlp3s0")
+
 wifiwidget = wibox.widget.textbox()
-vicious.register(wifiwidget, vicious.widgets.wifi, " ${ssid}", 3, "wlp3s0")
+vicious.register(wifiwidget, vicious.widgets.wifi, "${ssid}", 3, "wlp3s0")
